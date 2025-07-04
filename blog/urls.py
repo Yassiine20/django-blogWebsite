8a6217978b9e6497f4dd44e5cf84bc64f16f8729
yassine <yassine.chebbi@flouci.com>
2025-07-04 16:15:23 +0100

    Add user post management features~

--- blog/urls.py
+++ blog/urls.py
@@ -16,20 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CategoryViewSet, CommentViewSet
 from django.conf import settings
 from django.conf.urls.static import static
-
-router = DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'categories', CategoryViewSet)
-router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('blogapp.urls')),
 ]
 
-urlpatterns += router.urls
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
